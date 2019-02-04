e3dbb2e74600852ab307240414f954dc639dbd72
l181054meerabchishti <46877062+l181054meerabchishti@users.noreply.github.com>
2019-02-04 23:51:03 +0500

    Add files via upload

---
+++ Q1_l181054header.h
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+struct gitHubUser
+{
+
+	string firstName;
+	string userName;
+	string password;
+	string email;
+	int folderCount;
+	string* gitHubFolders;
+	string newfolder;
+	string *institute_name = new string;
+	string *qualification_level = new string;
+
+};
